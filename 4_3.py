057dcc9412145e67f2d59294e8ce9ebc34c7c6a6
ungitamas <166749667+ungitamas@users.noreply.github.com>
2024-04-18 19:31:09 +0200

    Feladat és melléklet

---
+++ 4_3.py
@@ -0,0 +1,5 @@
+# 4/3.	A mellékelt paszuly.txt szövegfájban az Égig érő paszuly című mese olvasható. Olvassa be a szövegfájlt tartalmát egy erre alkalmas adatszerkezetbe, majd oldja meg az alábbi feladatokat:
+# a.	Írja ki a képernyőre, hány sort olvasott be a fájlból!
+# b.	Írja ki, hogy a mese hány mondatból áll! (Minden mondat végén pont, kérdőjel vagy felkiáltójel található.)
+# c.	Hányféle ragozott formában fordul elő a paszuly szó a szövegben? Gyűjtse össze és írja ki az összes ragozott formát! Ügyeljen arra, hogy egy szót csak egyszer jelenítsen meg és a szavak ne tartalmazzák az őket követő írásjeleket (; , . ! ?)
+# d.	Írja ki, melyik a legrövidebb sor a fájlban (melyik sor tartalmazza a legkevesebb karaktert)! Jelenítse meg magát a sort és karaktereinek számát is!
