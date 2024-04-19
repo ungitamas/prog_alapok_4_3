2976f88db6c7fdea9a1e3c4ac5c8a85124b3672c
ungitamas <ungi.tamas@moriczref.hu>
2024-04-19 12:19:38 +0200

    2 es feladatig

--- 4_3.py
+++ 4_3.py
@@ -3,3 +3,17 @@
 # b.	Írja ki, hogy a mese hány mondatból áll! (Minden mondat végén pont, kérdőjel vagy felkiáltójel található.)
 # c.	Hányféle ragozott formában fordul elő a paszuly szó a szövegben? Gyűjtse össze és írja ki az összes ragozott formát! Ügyeljen arra, hogy egy szót csak egyszer jelenítsen meg és a szavak ne tartalmazzák az őket követő írásjeleket (; , . ! ?)
 # d.	Írja ki, melyik a legrövidebb sor a fájlban (melyik sor tartalmazza a legkevesebb karaktert)! Jelenítse meg magát a sort és karaktereinek számát is!
+
+mese = []
+
+with open("paszuly.txt" ,"r", encoding="utf-8") as bemenet: 
+    for sor in bemenet:
+        mese.append(sor.strip())
+
+print(f"{len(mese)} sort olvastunk be a fájlból") 
+
+mondat = 0
+for sor in mese: 
+    mondat += sor.count(".")+sor.count("?")+sor.count("!")
+
+print(f"A mese {mondat} mondatból áll")
